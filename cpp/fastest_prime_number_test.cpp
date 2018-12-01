1ac9272ec0e7dec82449fa1172da115b87014ea6
Yash Chandra Verma <ycverma005@gmail.com>
2018-12-01 17:18:30 +0530

    Add files via upload

---
+++ cpp/fastest_prime_number_test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    if(isPrime(n)){
+        cout<<"It is a Prime no";
+    }
+    else{
+        cout<<"It is not a Prime no.";
+    }
+return 0;
+}
