e2fc3d3e35b45a307df3cbcefb51e62e4ae891bd
Yash Chandra Verma <ycverma005@gmail.com>
2020-03-04 21:12:05 +0530

    remove dup val from row of col
    Removing duplicate value from the row of the particular column

---
+++ Python/remove_dup_col_csv.py
@@ -0,0 +1,38 @@
+# first you need to install a py module, run following command
+# pip install csv
+# importing csv module 
+import csv, numpy
+
+fields = []
+rows = []
+
+def to_dic(l):
+    l= l.strip('][').split(", ") 
+    l=[i.replace("'", "") for i in l]
+    l=list(set(l))
+    return l
+
+# reading csv file 
+with open(r"C:\Users\asus\Downloads\Edge\annotation.csv", 'r') as csvfile: 
+    # creating a csv reader object 
+    csvreader = csv.reader(csvfile)
+      
+    # extracting field names through first row 
+    fields = next(csvreader)
+  
+    # extracting each data row one by one 
+    for row in csvreader:
+        row[1] = to_dic(row[1])
+        rows.append(row) 
+
+# print(fields)
+# print(rows[:5])
+
+with open(r"C:\Users\asus\Downloads\Edge\food_dataset.csv", 'w',newline="") as csvfile: 
+    # creating a csv writer object 
+    csvwriter = csv.writer(csvfile) 
+    # writing the fields 
+    csvwriter.writerow(fields) 
+      
+    # writing the data rows 
+    csvwriter.writerows(rows)
